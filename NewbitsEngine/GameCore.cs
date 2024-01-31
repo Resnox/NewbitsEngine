00515627a56534410871643b90bd02eb7c3ebe00
resnox <bastien.rimbert@laposte.net>
2024-01-31 23:25:33 +0100

    init: Initial commit

---
+++ NewbitsEngine/GameCore.cs
@@ -0,0 +1,72 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using XnaGame = Microsoft.Xna.Framework.Game;
+
+namespace NewbitsEngine;
+
+public class GameCore : XnaGame
+{
+    public GameCore()
+    {
+        Gdm = new GraphicsDeviceManager(this);
+        Gdm.PreferredBackBufferWidth = 720;
+        Gdm.PreferredBackBufferHeight = 405;
+        Gdm.SynchronizeWithVerticalRetrace = true;
+        Gdm.IsFullScreen = false;
+
+        Window.AllowUserResizing = true;
+
+        Content.RootDirectory = "Content";
+
+        IsMouseVisible = false;
+        IsFixedTimeStep = false;
+    }
+
+    public GraphicsDeviceManager Gdm { get; }
+    public SpriteBatch SpriteBatch { get; private set; }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+
+        SpriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void UnloadContent()
+    {
+        base.UnloadContent();
+
+        SpriteBatch.Dispose();
+    }
+
+    protected override void OnExiting(object sender, EventArgs args)
+    {
+        base.OnExiting(sender, args);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        base.Draw(gameTime);
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        SpriteBatch.Begin(SpriteSortMode.Deferred,
+            BlendState.AlphaBlend,
+            SamplerState.PointClamp,
+            null,
+            null,
+            null,
+            Matrix.Identity //Camera.Transform
+        );
+        SpriteBatch.End();
+    }
+}
---
+++ NewbitsEngine/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace NewbitsEngine;
+
+internal class Program
+{
+    private const int LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000;
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetDefaultDllDirectories(int directoryFlags);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    private static extern void AddDllDirectory(string lpPathName);
+
+    [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool SetDllDirectory(string lpPathName);
+
+    public static void Main(string[] args)
+    {
+        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            try
+            {
+                SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+                AddDllDirectory(Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    Environment.Is64BitProcess ? "x64" : "x86"
+                ));
+            }
+            catch
+            {
+                SetDllDirectory(Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    Environment.Is64BitProcess ? "x64" : "x86"
+                ));
+            }
+
+        using var game = new GameCore();
+        game.Run();
+    }
+}
